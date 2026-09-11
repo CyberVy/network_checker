89f4cb1a2b8def40b183a8c62f154f56a5c71d3d
CyberVy <923353275@qq.com>
2026-09-12 01:00:56 +0800

    improve: mount react node after service worker is ready

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import type { CFResponseObject } from "@/core/cf_checker"
 import { LaunchAnimation } from "@/blocks/LaunchAnimation"
 import { CFChecker } from "@/blocks/CFChecker"
@@ -8,15 +8,6 @@ import { FullscreenModalContainer } from "@/components"
 export default function App(){
     const [show_launch_animation, set_show_launch_animation] = useState(true)
     const [cf_response_object_record, set_cf_response_object_record] = useState<Record<string, CFResponseObject | undefined>>({})
-
-    // register service worker
-    useEffect(() => {
-        if ('serviceWorker' in navigator){
-            navigator.serviceWorker.register('/sw.js').then(() => {
-                console.log('Service worker is registered successfully.')
-            }).catch(err => {console.error('Failed to register Service worker.', err)})
-        }
-    }, [])
 
     return (
         <>
